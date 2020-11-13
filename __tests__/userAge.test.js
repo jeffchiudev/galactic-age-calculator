035e6a60dd88c6fd25953a8c99762f24df99149f
Jeff Chiu <jwchiu82@gmail.com>
2020-11-13 09:23:41 -0800

    correctly pass user name and age entry

--- __tests__/userAge.test.js
+++ __tests__/userAge.test.js
@@ -5,6 +5,6 @@ describe('UserAge', () => {
   test('Should correctly intake a user and a users age', () => {
     const user1 = new UserAge("Jeff", 38);
     expect(user1.name).toEqual("Jeff");
-    expect(user1.earthAge).toEqual(39);
+    expect(user1.earthAge).toEqual(38);
   });
 })
